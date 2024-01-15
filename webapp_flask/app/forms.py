e04a6a412914f09162d99d334f746663abc5ac72
idobarakk <122865696+idobarakk@users.noreply.github.com>
2024-01-15 15:49:52 +0200

    Accounts +login

--- webapp_flask/app/forms.py
+++ webapp_flask/app/forms.py
@@ -1,7 +1,10 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileAllowed, FileField
+from flask_login import current_user
 from wtforms import StringField,PasswordField,SubmitField, BooleanField, IntegerField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from app.models import User
+
 
 
 class RegistrationForm(FlaskForm):
@@ -27,3 +30,27 @@ class LoginForms(FlaskForm):
     password = PasswordField('Password',validators=[DataRequired()])
     remember =BooleanField('Remember Me')
     submit = SubmitField('login')
+
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',validators=[DataRequired(),Length(min=2,max=20) ])
+    email = StringField('Email',validators=[DataRequired(),Email()])
+    picture = FileField("Update Profile Picture",validators=[FileAllowed(['jpg','png','jpeg'])])
+    submit = SubmitField('Update')
+
+    def validate_username (self,username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That Username is taken. Choose a diffrent one.')
+        else:
+            raise ValidationError('That Username must be diffrent than your current one.') 
+            
+    def validate_email (self,email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken. Choose a diffrent one.')
+        else:
+            raise ValidationError('That Username must be diffrent than your current one.')
