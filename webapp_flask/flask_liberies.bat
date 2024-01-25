c1c9f45a395fe5a7bd52f98acfc331807a0c42e1
idobarakk <122865696+idobarakk@users.noreply.github.com>
2024-01-25 20:18:12 +0200

    LIB

--- webapp_flask/flask_liberies.bat
+++ webapp_flask/flask_liberies.bat
@@ -6,6 +6,6 @@ pip install pillow
 pip install flask_wtf
 pip install flask_admin
 pip install flask-socketio
-pip install --upgrade python-socketio==4.6.0
-pip install --upgrade python-engineio==3.13.2
-pip install --upgrade Flask-SocketIO==4.3.1
+pip install --upgrade python-socketio
+pip install --upgrade python-engineio
+pip install --upgrade Flask-SocketIO
